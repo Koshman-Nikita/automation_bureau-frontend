4a804042e9da62277472bb9f4dd6aff5ec2e26a9
Koshman-Nikita <63288899+Koshman-Nikita@users.noreply.github.com>
2025-09-01 23:19:13 +0300

    Feat/fe reinit (#7)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,6 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  template: `<router-outlet />`
 })
 export class AppComponent {}
